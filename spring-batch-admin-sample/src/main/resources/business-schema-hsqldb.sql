59efacf1f96af5ad11e9847bf883200a07339d13
UE57996 <philipp.oser@sbb.ch>
2014-04-10 09:39:32 +0200

    Updated some library versions and made a quick release (for tests to pass in mvn it needed some hacks to get running: forkmode=true and some dummy sql scripts) - needs mvn clean install -DENVIRONMENT=hsql to build

---
+++ spring-batch-admin-sample/src/main/resources/business-schema-hsqldb.sql
@@ -0,0 +1,2 @@
+--just some stmt to avoid empty file error
+call current_timestamp;
---
+++ spring-batch-admin-sample/src/main/resources/business-schema-mysql.sql
@@ -0,0 +1,2 @@
+-- just some stmt to avoid empty file error
+select schema_name from information_schema.schemata where schema_name = 'DBName';
